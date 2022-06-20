9f343a61247ed2014eb9966ccd63dd4e554e6194
KATYA <kmazovs@bk.ru>
2022-06-20 21:41:42 +0300

    Вторая задача

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int n1, n2;
+
+Console.WriteLine("Введите первое число:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1<n2)
+{
+    Console.WriteLine ("Максимальное число:" + n2);
+}
+else 
+{
+    Console.WriteLine ("Максимальное число:" + n1);
+}
+
+
