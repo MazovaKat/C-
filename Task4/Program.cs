28b47e95987be9703f9b4b096ae8ded3ccfa88b5
KATYA <kmazovs@bk.ru>
2022-06-20 21:44:01 +0300

    четвертая задача

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int n1, n2, n3;
+
+Console.WriteLine("Введите первое число:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (max < n2) max = n2;
+if (max < n3) max = n3;
+
+Console.WriteLine ("Максимальное число:" + max);
