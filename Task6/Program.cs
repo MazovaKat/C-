ad7ab993d53b05b96fa0d2e7a604dd00e3071e7c
KATYA <kmazovs@bk.ru>
2022-06-20 21:47:54 +0300

    Шестая задача

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿int n1;
+
+Console.WriteLine("Введите число:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % 2 == 0)
+{
+   Console.WriteLine ("Число четное"); 
+}
+else 
+{
+    Console.WriteLine ("Число нечетное");
+}
+
