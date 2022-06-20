8db9d2034c641ec30800c266757bb10032702b2e
KATYA <kmazovs@bk.ru>
2022-06-20 22:17:55 +0300

    Восьмая задача

---
+++ Task8_/Program.cs
@@ -0,0 +1,18 @@
+﻿int n1;
+
+Console.WriteLine("Введите число:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+
+int i = 2;
+
+            while (i <= n1)
+                {
+                if (i%2==0)
+                    {
+                        Console.Write(i);
+                    }
+             i++;
+         }
+        
+
