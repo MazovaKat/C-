92ea412e41ae571cf49d53cb69f28bb3152ba093
KATYA <kmazovs@bk.ru>
2022-06-27 20:50:37 +0300

    десятая задача

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿int FindSecondDigit (int num)
+{
+    int d = num / 10;
+    while ( d > 10)
+    { 
+        d = d - 10;
+    }
+
+    return d;
+}
+
+Console.WriteLine("Введите трехзначное число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int second = FindSecondDigit (n);
+
+Console.WriteLine("Вторая цифра это " + second);
+
+
