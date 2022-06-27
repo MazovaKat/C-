d1ec14d08d14701d4776278535922bf2a50e43f7
KATYA <kmazovs@bk.ru>
2022-06-27 21:45:18 +0300

    Тринадцатая задача

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿int FindThirdDigit (int num)
+{   
+    int third = 0;
+    if (num >= 100)
+        {
+            while (num >= 1000)
+                    {
+                         num = num / 10;
+                    }
+            third = num%10;
+        }
+    else
+        {
+            return -1;
+        }
+
+return third;  
+}
+  
+
+Console.WriteLine("Введите число");
+
+int n = Convert.ToInt32(Console.ReadLine());  
+int th = FindThirdDigit(n);
+
+Console.WriteLine(th);
