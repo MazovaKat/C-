8fa7d9a1aac23193fe8e13fdb7fe45426d38e819
KATYA <kmazovs@bk.ru>
2022-06-28 12:20:11 +0300

    пятнадцатая задача

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿void HappyWeekend (int day)
+{
+    if (day<6)
+        {
+            Console.WriteLine ("Рабочий день");
+        }
+       else 
+        {
+            Console.WriteLine ("Выходной");
+        }
+}
+
+Console.WriteLine("Введите день недели:");
+int n = Convert.ToInt32(Console.ReadLine());  
+
+HappyWeekend(n);
+
