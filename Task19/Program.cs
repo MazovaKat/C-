cda78d399d7c0fb6516b27640e82285eb21b859b
KATYA <kmazovs@bk.ru>
2022-06-30 20:02:48 +0300

    девятнадцатая задача

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿void Palindrom ( int x)
+{
+    int b = 0;
+    int a = x;
+        while ( a%10 > 1 )
+            {
+             b = (b*10) + a%10;
+             a = a/10;
+            }
+    b = (b*10) + a;
+
+    if ( b == x)
+    { 
+        Console.WriteLine("Число палиндром");
+    }
+    else 
+    {
+        Console.WriteLine("Число не палиндром");
+    }
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Palindrom(num);
