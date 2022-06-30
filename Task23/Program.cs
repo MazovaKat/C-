df69ddda0e94b9ab643eff95bc563f2635620242
KATYA <kmazovs@bk.ru>
2022-06-30 20:37:56 +0300

    задача 23

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿void Cube (int n)
+{
+    
+    for ( int x=1; x<=n; x++)
+    {
+        Console.Write(x*x*x + " ");
+    }
+}
+
+Console.WriteLine ("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Cube (num);
