e35308e6862cc782315aa64ca9a0ab0c15ff8128
KATYA <kmazovs@bk.ru>
2022-07-05 19:34:34 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿int Sum(int x)
+{
+    int sum = 0;
+        while ( x%10 > 1/10 )
+            {
+             sum = sum + x%10;
+             x = x/10;
+            }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int summa = Sum(num);
+
+Console.WriteLine(summa);
