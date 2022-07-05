10c37764245848d685c44e12c2ef7be40a6b67e0
KATYA <kmazovs@bk.ru>
2022-07-05 19:54:00 +0300

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿void NewArr (int[] array)
+{
+    int count = array.Length;
+    int index = 0;
+
+        while (index < count)
+        {
+            array[index] = new Random().Next(1,10);
+            Console.Write (array[index]);
+            index ++;
+        }
+
+}
+
+int[] newArray = new int[8];
+
+NewArr(newArray);
