088f7ff0d06d3979aa32c6d1f6e929ec7fad65df
KATYA <kmazovs@bk.ru>
2022-07-23 20:42:29 +0300

    задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿int count (int n)
+{
+    if (n/10!=0)
+        return 1+count(n/10);
+    else
+        return 1;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(count(num));
