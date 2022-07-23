d6d86d588ae8e8854a021072f75694bb13ea18fb
KATYA <kmazovs@bk.ru>
2022-07-23 21:34:12 +0300

    задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿int SumOfNum (int m, int n)
+{
+    int sum =m;
+    if (m>n)
+    {
+       sum = sum +(SumOfNum(m-1,n));
+       n = n+1;
+    }
+    return sum;
+}
+
+Console.Write("Введите число M (большее из чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N (меньшее из чисел): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfNum(m,n));
