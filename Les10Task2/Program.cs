f4a26084823807be7fde4cc3a8fffdaffeea57aa
KATYA <kmazovs@bk.ru>
2022-07-28 20:42:42 +0300

    Семинар 10 Задача 2

---
+++ Les10Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] NewNames(string[] words) 
+{
+    string[] New = new string[words.Length/2];
+    int j = 0;
+    for (int i = 0; i <= words.Length-2; i=i+2)
+    {   
+        New[j] = words[i] + words[i+1];
+        j++;
+        
+    }
+    return New;
+}    
+void Show (string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        Console.Write(a[i]+ " ");
+}
+
+
+string[] names = {"qwe", "wer", "ert", "rty", "tyu", "yui"};
+
+Show(NewNames(names));
