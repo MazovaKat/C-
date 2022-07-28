192377c9f9207bcd95e9c9098d46acbb9d02f8fd
KATYA <kmazovs@bk.ru>
2022-07-28 20:09:41 +0300

    10 семинар 1 задача

---
+++ Les10Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int FindWordCount(string[] words, string[] letter) 
+{
+    int count = 0;
+    for (int i = 0; i < words.Length; i++)
+    {   for (int j = 0; j < letter.Length; j++)
+        if(words[i][0] == letter[j][0]) 
+        count = count + 1;
+    }
+    return count;
+}
+
+string[] names = {"qwe", "wer", "ert", "rty", "tyu"};
+string[] letters = {"a", "e", "i", "o", "u", "y"};
+
+Console.WriteLine(FindWordCount(names, letters));
+
+
